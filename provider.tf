18848c31e09a93da7fd567e073a126410450e7bf
chinazam C. Dennar <exchangehub@aol.com>
2024-02-05 01:30:09 -0700

    network commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = var.aws_access_key
+    access_key = var.aws_access_key_id
     region     = var.region
-    secret_key = var.aws_secret_key
+    secret_key = var.aws_secret_access_key
 }
--- variable.tf
+++ variable.tf
@@ -15,10 +15,10 @@ variable "region" {
   type = string
 }
 
-variable "aws_access_key" {
+variable "aws_access_key_id" {
   type = string
 }
 
-variable "aws_secret_key" {
+variable "aws_secret_access_key" {
   type = string
 }
