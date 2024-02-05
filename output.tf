b91783ad9689d3c309557806bee23e844a42ce21
chinazam C. Dennar <exchangehub@aol.com>
2024-02-05 01:23:36 -0700

    network commit

--- output.tf
+++ output.tf
@@ -3,21 +3,18 @@ output "vpc_id" {
 }
 
 output "external_subnet_ids" {
-  value = aws_subnet.external_subnets[*].id
+  value = [for subnet in aws_subnet.external_subnets : subnet.id]
 }
 
 output "internal_subnet_ids" {
-  value = aws_subnet.internal_subnets[*].id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.my_igw.id
+  value = [for subnet in aws_subnet.internal_subnets : subnet.id]
 }
 
 output "external_route_table_ids" {
-  value = aws_route_table.external_route_tables[*].id
+  value = [for rt in aws_route_table.external_route_tables : rt.id]
 }
 
 output "internal_route_table_ids" {
-  value = aws_route_table.internal_route_tables[*].id
+  value = [for rt in aws_route_table.internal_route_tables : rt.id]
 }
+
